95557836795bda9e1d1ce1aaa7f5313e6427b1e7
Han Kyu Yoo <hanquel85@gmail.com>
2023-08-31 16:51:40 +0900

    pod 설정 변경, p 화면 레이아웃 수정

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,13 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils.dart';
 // import 'package:myapp/page-1/c-1.dart';
 // import 'package:myapp/page-1/c-0.dart';
-// import 'package:myapp/page-1/p-0.dart';
 // import 'package:myapp/page-1/c-2.dart';
-// import 'package:myapp/page-1/p-1.dart';
+
+// import 'package:myapp/page-1/p-0.dart';
+import 'package:myapp/page-1/p-1.dart';
+
 // import 'package:myapp/page-1/m-3.dart';
-import 'package:myapp/page-1/m-2.dart';
+// import 'package:myapp/page-1/m-2.dart';
 // import 'package:myapp/page-1/m-1.dart';
 // import 'package:myapp/page-1/m-0.dart';
 
